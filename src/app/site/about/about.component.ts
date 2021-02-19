700994d136ffbd48339a48ce040243a260897752
Md Abu Sayeed Chowdhury Abir <abir.sayeed@credencesoft.co.nz>
2021-02-19 16:15:20 +0600

    update

--- src/app/site/about/about.component.ts
+++ src/app/site/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorage } from 'src/app/token.storage';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(public token :TokenStorage) { }
 
   ngOnInit() {
   }
--- src/app/site/home/aboutus/aboutus.component.ts
+++ src/app/site/home/aboutus/aboutus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorage } from 'src/app/token.storage';
 
 @Component({
   selector: 'app-aboutus',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  constructor(public token :TokenStorage) { }
 
   ngOnInit() {
   }
