d4efb1d01e5f747bf65cdb4f7b09773dec54061e
sunil-credencesoft <80749734+sunil-credencesoft@users.noreply.github.com>
2021-03-20 11:26:00 +0530

    Images fixed

--- src/app/pages/home/notch/notch.component.ts
+++ src/app/pages/home/notch/notch.component.ts
@@ -6,39 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notch.component.css']
 })
 export class NotchComponent implements OnInit {
-  galleryItems=[
-{
-imageUrl:'assets/images/service_img1.jpg',
-title:'Top-Notch'
-},
-{
-  imageUrl:'assets/images/service_img2.jpg',
-  title:'Top-Notch'
-  },
-  {
-    imageUrl:'assets/images/service_img3.jpg',
-    title:'Top-Notch'
+  galleryItems = [
+    {
+      imageUrl: 'assets/images/service_img1.jpg',
+      title: 'Top-Notch'
     },
     {
-      imageUrl:'assets/images/service_img4.jpg',
-      title:'Top-Notch air'
-      },
-      {
-        imageUrl:'assets/images/service_img5.jpg',
-        title:'Top-Notch air'
-        },
-        {
-          imageUrl:'assets/images/service_img6.jpg',
-          title:'Lets Talk'
-          },
-          {
-            imageUrl:'assets/images/service_img7.jpg',
-            title:'Lets Talk'
-            },
-            {
-              imageUrl:'assets/images/service_img8.jpg',
-              title:'Lets Talk'
-              }
+      imageUrl: 'assets/images/service_img2.jpg',
+      title: 'Top-Notch'
+    },
+    {
+      imageUrl: 'assets/images/service_img3.jpg',
+      title: 'Top-Notch'
+    },
+    {
+      imageUrl: 'assets/images/service_img4.jpg',
+      title: 'Top-Notch air'
+    },
+    {
+      imageUrl: 'assets/images/service_img5.jpg',
+      title: 'Top-Notch air'
+    },
+    {
+      imageUrl: 'assets/images/service_img6.jpg',
+      title: 'Lets Talk'
+    },
+    {
+      imageUrl: 'assets/images/service_img7.jpg',
+      title: 'Lets Talk'
+    },
+    {
+      imageUrl: 'assets/images/service_img8.jpg',
+      title: 'Lets Talk'
+    }
 
   ]
   constructor() { }
