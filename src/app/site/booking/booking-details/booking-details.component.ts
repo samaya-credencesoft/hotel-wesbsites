a9e06e55427fbde47000c0a4a4733a7d13ab0420
Md Abu Sayeed Chowdhury Abir <53491180+Abir-CredenceSoft@users.noreply.github.com>
2021-03-30 14:15:12 +0600

    fix

--- src/app/site/booking/booking-details/booking-details.component.ts
+++ src/app/site/booking/booking-details/booking-details.component.ts
@@ -1,11 +1,12 @@
 import { TokenStorage } from 'src/app/token.storage';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { ApiService } from './../../../api.service';
 import { DateService } from '../../../date-service.service';
 import { Booking } from 'src/app/model/booking';
 import { Payment } from 'src/app/model/payment';
+import { DateModel } from 'src/app/model/dateModel';
 
 
 @Component({
@@ -14,6 +15,7 @@ import { Payment } from 'src/app/model/payment';
   styleUrls: ['./booking-details.component.css']
 })
 export class BookingDetailsComponent implements OnInit {
+  dateModel:DateModel;
   currency: string;
   booking: Booking ;
   bookingEmail: string ;
@@ -29,6 +31,7 @@ export class BookingDetailsComponent implements OnInit {
       public token: TokenStorage,
       private activatedRoute: ActivatedRoute,
       public dateService : DateService,
+      private changeDetectorRefs: ChangeDetectorRef,
       private apiServices: ApiService) {
 
         this.booking = new Booking();
@@ -58,15 +61,47 @@ export class BookingDetailsComponent implements OnInit {
       this.apiServices.getBookingDetailsByIdAndEmail(this.booking).subscribe( response => {
        this.booking = response.body.bookingDetails ;
        this.payments = response.body.paymentDetails;
+       this.payment = this.payments[0];
        this.loader = false;
 
        this.currency = this.booking.currency;
-      //  console.log(this.booking);
-      //  console.log(this.payments);
+
+       this.changeDetectorRefs.detectChanges();
+       console.log(this.booking);
+       console.log(this.payment);
       },error=>{ this.loader = false;});
     }
     onGoHome() {
       this.router.navigate(["/"]);
       // this.locationBack.back();
     }
+    getAvailableRoom() {
+      this.dateModel = new DateModel();
+
+      this.dateModel.checkIn = this.getDateFormat(this.booking.fromDate);
+      this.dateModel.checkOut = this.getDateFormat(this.booking.toDate);
+      this.dateModel.guest = this.booking.noOfPersons;
+      this.dateModel.noOfRooms = this.booking.noOfRooms;
+
+      // console.log(' this.dateModel '+JSON.stringify( this.dateModel));
+
+      const navigationExtras: NavigationExtras = {
+        queryParams: {
+          dateob: JSON.stringify(this.dateModel),
+        },
+      };
+
+      this.router.navigate(["/booking/choose"], navigationExtras);
+    }
+    getDateFormat(dateString: string) {
+      var yearAndMonth = dateString.split("-", 3);
+      return (
+        yearAndMonth[0] +
+        "-" +
+        yearAndMonth[1] +
+        "-" +
+        yearAndMonth[2].split(" ", 1)
+      );
+    }
+
   }
